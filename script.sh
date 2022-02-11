bceb9b892437ab1ab58bbec0bdda536e279c827a
rhea-2000 <97457769+rhea-2000@users.noreply.github.com>
2022-02-11 21:54:38 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -16,6 +16,6 @@ sed -i "s#NAME#$NAME#g" task-definition.json
 
 aws ecs register-task-definition --cli-input-json file://task-definition.json --region="${AWS_DEFAULT_REGION}"
 
-REVISION='"aws ecs describe-task-definition --task-definition node-task --region us-east-1 | jq .taskDefinition.revision"'
+REVISION='aws ecs describe-task-definition --task-definition node-task --region us-east-1 | jq .taskDefinition.revision'
 echo "REVISION= " "${REVISION}"
 aws ecs update-service --cluster node-js-app --service node-js-r --task-definition node-task:1 --desired-count 1
