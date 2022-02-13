319df867b36bdc31a34db280646377e651caaa5d
rhea-2000 <97457769+rhea-2000@users.noreply.github.com>
2022-02-13 16:37:20 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -14,6 +14,6 @@ sed -i "s#FAMILY#$FAMILY#g" task-definition.json
 sed -i "s#NAME#$NAME#g" task-definition.json
 
 
-aws ecs register-task-definition --cli-input-json file://task-definition.json --region=us-east-1
+
 
 
